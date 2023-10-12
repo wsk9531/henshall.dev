a0e90ad4169ad31f819a7bb80f1280724da49337
ben-wsk9531 <67457411+wsk9531@users.noreply.github.com>
2023-10-13 00:52:09 +0100

    feat: add ssg serve as cli command

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
 
+const (
+	OUTPUT_DIR string = "dist"
+	DEFAULT_PORT string = "5000"
+	
+)
 func main() {
-	fmt.Println("Hello world!")
+	generateCmd := flag.NewFlagSet("generate", flag.ExitOnError)
+	var contentDir string
+	generateCmd.StringVar(&contentDir, "input", "pages", "path to content for rendering")
+
+	serveCmd := flag.NewFlagSet("serve", flag.ExitOnError)
+	var port string
+	serveCmd.StringVar(&port, "port", DEFAULT_PORT, "localhost port to serve content")
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'generate' or 'serve' subcommands")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+
+	case "generate":
+		generateCmd.Parse(os.Args[2:])
+		fmt.Println("subcommand 'generate'")
+		fmt.Println("  dir:", contentDir)
+		fmt.Println("  tail:", generateCmd.Args())
+
+		// do stuff
+
+	case "serve":
+		serveCmd.Parse(os.Args[2:])
+
+		url, err := parseLocalIPAddr(port)
+		if err != nil {
+			log.Fatal(err)
+		}
+		addr := url.Host
+
+		log.Printf("Starting server on: %s", addr)
+		log.Fatal(http.ListenAndServe(addr, http.FileServer(http.FS(os.DirFS(OUTPUT_DIR)))))
+
+	default:
+		fmt.Println("expected 'generate' or 'serve' subcommands")
+		os.Exit(1)
+	}
+}
+
+func parseLocalIPAddr(port string) (*url.URL, error) {
+	u, err := url.Parse(fmt.Sprintf("https://127.0.0.1:%s", port))
+	if err != nil {
+		fmt.Println(err)
+	}
+	return u, nil
 }
