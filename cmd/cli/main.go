bf894b53ec1d0fe331703ca5412874c215ae8959
ben-wsk9531 <67457411+wsk9531@users.noreply.github.com>
2023-10-13 23:41:19 +0100

    feat: add new markdown renderer; add WIP happy-path generate command

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
@@ -8,23 +9,23 @@ import (
 	"net/url"
 	"os"
 
-	_ "github.com/wsk9531/henshall.dev/internal/generator"
+	"github.com/wsk9531/henshall.dev/internal/generator"
 )
 
 const (
-	CONTENT_DIR string = "pages"
-	OUTPUT_DIR string = "dist"
-	DEFAULT_PORT string = "5000"
+	DEFAULT_CONTENT_DIR string = "pages"
+	DEFAULT_OUTPUT_DIR  string = "dist"
+	DEFAULT_PORT        string = "5000"
 )
 
 func main() {
 	generateCmd := flag.NewFlagSet("generate", flag.ExitOnError)
-	var contentDir string
-	generateCmd.StringVar(&contentDir, "input", CONTENT_DIR, "path to content for rendering")
+	contentDir := *generateCmd.String("content", DEFAULT_CONTENT_DIR, "path to content for rendering")
+	outputDir := *generateCmd.String("output", DEFAULT_OUTPUT_DIR, "path to rendered site files")
 
 	serveCmd := flag.NewFlagSet("serve", flag.ExitOnError)
-	var port string
-	serveCmd.StringVar(&port, "port", DEFAULT_PORT, "localhost port to serve content")
+	serveDir := *serveCmd.String("dir", DEFAULT_OUTPUT_DIR, "path to rendered site files")
+	port := *serveCmd.String("port", DEFAULT_PORT, "localhost port to serve content")
 
 	if len(os.Args) < 2 {
 		fmt.Println("expected 'generate' or 'serve' subcommands")
@@ -34,32 +35,57 @@ func main() {
 	switch os.Args[1] {
 
 	case "generate":
-		generateCmd.Parse(os.Args[2:])
+		generateCmd.Parse(os.Args[2:]) //nolint:errcheck // this error is handled by the Flags' parser fail behaviour for now
+
 		fmt.Println("subcommand 'generate'")
-		fmt.Println("  dir:", contentDir)
-		fmt.Println("  tail:", generateCmd.Args())
+		fmt.Println(" content dir:", contentDir)
+		fmt.Println(" output dir:", outputDir)
 
-		// TODO: Add generate cli subcommand
-		// e.g.
-		// posts, _ := generator.NewPostsFromFS(os.DirFS(contentDir))
-		// log.Println(posts)
-		//postRenderer, _ := generator.NewPostRenderer()
-		// for _, page := range(posts) {
-		// 	postRenderer.Render(page)
-		// }
+		err := os.Mkdir(outputDir, 0750)
+		if err != nil && !os.IsExist(err) {
+			log.Fatal(err)
+		}
 
+		posts, err := generator.NewPostsFromFS(os.DirFS(contentDir))
+		if err != nil {
+			log.Fatal(err)
+		}
+		postRenderer, err := generator.NewPostRenderer()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, page := range posts {
+			// Render content into buffer
+			buf := bytes.Buffer{}
+			if err := postRenderer.Render(&buf, page); err != nil {
+				log.Fatal(err)
+			}
+
+			// write buffer to file
+			path := outputDir + "/" + page.SanitisedTitle() + ".html"
+			err = os.WriteFile(path, buf.Bytes(), 0660)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 
 	case "serve":
-		serveCmd.Parse(os.Args[2:])
-
+		serveCmd.Parse(os.Args[2:]) //nolint:errcheck // this error is handled by the Flags' parser fail behaviour for now
+		fmt.Println("subcommand 'serve'")
+		fmt.Println(" site content dir:", serveDir)
+		fmt.Println(" port:", port)
 		url, err := parseLocalURL(port)
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		addr := url.Host
+		fs := http.FS(os.DirFS(serveDir))
 
 		log.Printf("Starting server on: %s", addr)
-		log.Fatal(http.ListenAndServe(addr, http.FileServer(http.FS(os.DirFS(OUTPUT_DIR)))))
+
+		log.Fatal(http.ListenAndServe(addr, http.FileServer(fs)))
 
 	default:
 		fmt.Println("expected 'generate' or 'serve' subcommands")
